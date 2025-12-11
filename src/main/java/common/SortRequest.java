cbf29d57e862475357614b014c59c00c0fb7fb93
TsvetanValchinov <cecovalchinov@abv.bg>
2025-12-11 21:27:09 +0200

    fixed warnings

--- src/main/java/common/SortRequest.java
+++ src/main/java/common/SortRequest.java
@@ -1,9 +1,11 @@
 package common;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public class SortRequest implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private int[] data;
